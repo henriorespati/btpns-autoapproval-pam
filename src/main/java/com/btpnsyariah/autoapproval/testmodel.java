0fa7aecaea021f024e678cf3d6b2edcfe654cb5f
adminUser <>
2020-10-16 14:08:17 +0000

    {/src/main/java/com/btpnsyariah/autoapproval/testmodel.java}

--- src/main/java/com/btpnsyariah/autoapproval/testmodel.java
+++ src/main/java/com/btpnsyariah/autoapproval/testmodel.java
@@ -8,21 +8,21 @@ public class testmodel implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	private java.util.Date date;
+	private java.util.Date birthDate;
 
 	public testmodel() {
 	}
 
-	public java.util.Date getDate() {
-		return this.date;
+	public java.util.Date getBirthDate() {
+		return this.birthDate;
 	}
 
-	public void setDate(java.util.Date date) {
-		this.date = date;
+	public void setBirthDate(java.util.Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
-	public testmodel(java.util.Date date) {
-		this.date = date;
+	public testmodel(java.util.Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
 }
